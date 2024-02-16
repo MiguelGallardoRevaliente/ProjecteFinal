39326cc0896382300cfdb1238617eacf3695ae2d
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-02-16 09:10:33 +0100

    Tope cambios

--- programacion/index.js
+++ programacion/index.js
@@ -1,17 +1,24 @@
 import express from 'express'
+import cors from 'cors'
+import path from 'path'
 
 const app = express()
+app.use(express.json())
+app.use(cors())
 app.disable('x-powered-by')
 
-const PORT = process.env.PORT ?? 1234
-
 app.get('/', (req, res) => {
-  res.send('Hello, world!')
+  res.header('Allow-Control-Allow-Origin', '*')
+  res.send('Paleto')
 })
 
 app.post('/', (req, res) => {
   console.log(req.body)
 })
+
+const PORT = process.env.PORT ?? 1234
+
+app.use(express.static(path.join('./web/index.html', 'public')))
 
 app.listen(PORT, () => {
   console.log('server listening on port http://localhost:' + PORT)
