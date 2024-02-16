8a0b923e4a3fd2213ec146adf49408cd6105bb2d
Mazapan39 <126164380+Mazapan39@users.noreply.github.com>
2024-02-16 08:44:30 +0100

    Max-Min

--- programacion/js/index.js
+++ programacion/js/index.js
@@ -8,3 +8,32 @@ function showRegister() {
     // Muestra el contenedor de registro
     registerContainer.style.display = 'block';
 }
+
+        var fullscreenIcon = document.getElementById('fullscreenIcon');
+
+        // Función para alternar entre pantalla completa y salida de pantalla completa
+        function toggleFullscreen() {
+            if (!document.fullscreenElement) {
+                document.documentElement.requestFullscreen();
+                fullscreenIcon.classList.add('fullscreen');
+                fullscreenIcon.classList.remove('exitedFullscreen');
+            } else {
+                document.exitFullscreen();
+                fullscreenIcon.classList.remove('fullscreen');
+                fullscreenIcon.classList.add('exitedFullscreen');
+            }
+        }
+
+        // Asocia la función al evento de clic en el icono
+        fullscreenIcon.addEventListener('click', toggleFullscreen);
+
+        // Escucha el evento de cambio en la pantalla completa para actualizar el icono
+        document.addEventListener('fullscreenchange', function () {
+            if (document.fullscreenElement) {
+                fullscreenIcon.classList.add('fullscreen');
+                fullscreenIcon.classList.remove('exitedFullscreen');
+            } else {
+                fullscreenIcon.classList.remove('fullscreen');
+                fullscreenIcon.classList.add('exitedFullscreen');
+            }
+        });
