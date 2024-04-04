5670456e64c7f358403b542e80ea54db659e3043
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-04-04 11:53:43 +0200

    hyju

--- programacion/web/js/game.js
+++ programacion/web/js/game.js
@@ -22,7 +22,7 @@ async function mostrarInventory () {
   inventory.style.display = 'block'
   settings.style.display = 'none'
   const yourCards = document.getElementsByClassName('yourCards')[0]
-  fetch('/cards', {
+  fetch('http://13.53.190.234/cards', {
     method: 'GET'
   })
     .then(response => response.json())
