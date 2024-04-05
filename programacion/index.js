0f1dd09a9d2acbd7cdb35be16ccbe5535875e70e
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-04-05 11:35:59 +0200

    a ver

--- programacion/index.js
+++ programacion/index.js
@@ -56,8 +56,9 @@ app.get('/inventory', async (req, res) => {
 app.get('/cards', async (req, res) => {
   try {
     const id = req.query.id
+    console.log(id)
     const idCartas = await connection.query(
-      'SELECT * FROM users_cartas WHERE UUID_TO_BIN(id_user) = ?', [id]
+      'SELECT * FROM users_cartas WHERE BIN_TO_UUID(id_user) = ?', [id]
     )
 
     console.log(idCartas)
