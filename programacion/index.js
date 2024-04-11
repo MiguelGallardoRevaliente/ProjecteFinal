c04e7018b428b2ee6267339b63dabd61023a997b
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-04-11 13:09:45 +0200

    Update index.js

--- programacion/index.js
+++ programacion/index.js
@@ -163,7 +163,7 @@ app.get('/getCardsDeck', async (req, res) => {
   try {
     const mazo = req.query.mazo
     const [cartasMazo] = await connection.execute('SELECT id_carta FROM mazo_cartas WHERE id_mazo = ?;', [mazo])
-    const [cards] = await connection.execute('SELECT * FROM cartas WHERE id != ?;', [cartasMazo.id_carta])
+    const [cards] = await connection.execute('SELECT * FROM cartas WHERE id NOT IN ?;', [cartasMazo.id_carta])
     return res.status(200).json(cards)
   } catch (err) {
     console.error(err)
