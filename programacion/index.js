01dc67219f26eef6b6c4ddb0f6b15720523dcb3a
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-04-11 12:01:26 +0200

    Update index.js

--- programacion/index.js
+++ programacion/index.js
@@ -120,7 +120,8 @@ app.get('/getDecks', async (req, res) => {
     const id = req.query.id
     const [mazoActual] = await connection.execute('SELECT * FROM users WHERE BIN_TO_UUID(id) = ?', [id])
     const [decks] = await connection.execute('SELECT * FROM mazos WHERE BIN_TO_UUID(id_user) = ? AND numero = ?', [id, mazoActual[0].mazo_seleccionado])
-    const [mazoCartas] = await connection.execute('SELECT id_carta FROM mazo_cartas WHERE id_mazo = ?', [decks[0].id])
+    let [mazoCartas] = await connection.execute('SELECT id_carta FROM mazo_cartas WHERE id_mazo = ?', [decks[0].id])
+    mazoCartas = Array.isArray(mazoCartas) ? mazoCartas : []
     console.log(mazoCartas)
     await Promise.all(mazoCartas.forEach(async (carta) => {
       console.log('Manolo: ' + carta.id_carta)
