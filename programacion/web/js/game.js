61c4ecff294260ba65c10a4deee45635d950f943
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-04-15 14:03:56 +0200

    Update game.js

--- programacion/web/js/game.js
+++ programacion/web/js/game.js
@@ -219,6 +219,7 @@ function salirMostrarCartas () {
   })
 
   if (!foundDuplicate) {
+    localStorage.setItem('cartas', [])
     window.location.reload()
   } else {
     alert('Hay cartas duplicada')
