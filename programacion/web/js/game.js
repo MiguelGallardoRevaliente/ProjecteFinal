0d954dbc8daa4eede6cce6ab6635c0dc8c8d888f
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-04-15 09:43:21 +0200

    a ve

--- programacion/web/js/game.js
+++ programacion/web/js/game.js
@@ -103,6 +103,7 @@ function getCartas (mazoActual, idCartaMazo) {
   const cartas = document.getElementsByClassName('cartas')[0]
   cartasNoMazo.style.display = 'flex'
   cartas.innerHTML = ''
+  document.body.style.overflow = 'hidden'
   fetch(`/getCardsDeck?mazo=${mazoActual}`, {
     method: 'GET'
   })
