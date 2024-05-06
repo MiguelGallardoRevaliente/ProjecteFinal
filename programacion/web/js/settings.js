5315d1b2442d84930e6533ebdcd91660604acf1a
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-05-06 12:18:05 +0200

    a

--- programacion/web/js/settings.js
+++ programacion/web/js/settings.js
@@ -1,32 +1,37 @@
 /* eslint-disable no-unused-vars */
 function editPassword () {
+  const changePasswordDiv = document.getElementsByClassName('changePassword')[0]
+  changePasswordDiv.innerHTML = ''
+
+  const passwordInput = document.createElement('input')
+  passwordInput.setAttribute('type', 'password')
+  passwordInput.className = 'password_input'
+  passwordInput.name = 'password'
+
+  const saveButton = document.createElement('button')
+  saveButton.className = 'save_button'
+  saveButton.innerHTML = "<i class='bx bx-check'></i>"
+  saveButton.setAttribute('onclick', 'savePassword()')
+
+  const cancelButton = document.createElement('button')
+  cancelButton.className = 'cancel_button'
+  cancelButton.innerHTML = "<i class='bx bx-x'></i>"
+  cancelButton.setAttribute('onclick', 'cancelPassword()')
+
+  changePasswordDiv.appendChild(passwordInput)
+  changePasswordDiv.appendChild(saveButton)
+  changePasswordDiv.appendChild(cancelButton)
+}
+
+function showPassword () {
   const id = localStorage.getItem('id')
   fetch('/password/' + id, {
     method: 'GET'
   })
-    .then(response => response.text())
+    .then(response => response.json())
     .then(data => {
-      const changePasswordDiv = document.getElementsByClassName('changePassword')[0]
-      changePasswordDiv.innerHTML = ''
-
-      const passwordInput = document.createElement('input')
-      passwordInput.setAttribute('type', 'password')
-      passwordInput.className = 'password_input'
-      passwordInput.name = 'password'
-      passwordInput.value = data
-
-      const saveButton = document.createElement('button')
-      saveButton.className = 'save_button'
-      saveButton.innerHTML = "<i class='bx bx-check'></i>"
-      saveButton.setAttribute('onclick', 'savePassword()')
-
-      const cancelButton = document.createElement('button')
-      cancelButton.className = 'cancel_button'
-      cancelButton.innerHTML = "<i class='bx bx-x'></i>"
-      cancelButton.setAttribute('onclick', 'cancelPassword()')
-
-      changePasswordDiv.appendChild(passwordInput)
-      changePasswordDiv.appendChild(saveButton)
-      changePasswordDiv.appendChild(cancelButton)
+      const password = data.password
+      const passwordLabel = document.getElementsByClassName('password_label')[0]
+      passwordLabel.innerHTML = password
     })
 }
