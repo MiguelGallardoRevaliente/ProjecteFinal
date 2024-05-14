fc81654db269a0b09c01d0f44fa8a44b9cd7b573
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-05-14 12:50:21 +0200

    Update index.js

--- programacion/index.js
+++ programacion/index.js
@@ -70,9 +70,14 @@ io.on('connection', async (socket) => {
     if (userSearching.length > 0) {
       await connection.execute('UPDATE users SET searching = 0, fighting = 1 WHERE user = ?', [data.username])
       await connection.execute('UPDATE users SET searching = 0, fighting = 1 WHERE user = ?', [userSearching[0].user])
-      await connection.execute('INSERT INTO combates (id_user_1, id_user_2) VALUES (UUID_TO_BIN(?), UUID_TO_BIN(?))', [data.id, userSearching[0].id_uuid])
+
+      // Insertar el combate
+      const [insertResult] = await connection.execute('INSERT INTO combates (id_user_1, id_user_2) VALUES (UUID_TO_BIN(?), UUID_TO_BIN(?))', [data.id, userSearching[0].id_uuid])
+
+      // Obtener el último ID insertado
       const [lastID] = await connection.execute('SELECT LAST_INSERT_ID()')
       const lastInsertedID = lastID[0]['LAST_INSERT_ID()']
+
       console.log(lastInsertedID)
       console.log('User1: ', data.username)
       console.log('User2: ', userSearching[0].user)
