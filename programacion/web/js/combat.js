be86b3055399a2dd000fbf6699a18130ddf1cc71
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-05-15 11:55:40 +0200

    a

--- programacion/web/js/combat.js
+++ programacion/web/js/combat.js
@@ -1,14 +1,13 @@
 /* eslint-disable no-unused-vars */
 function dragStart (event) {
   event.dataTransfer.setData('text', event.target.id)
-  event.dataTransfer.setData('src', event.target.src)
   console.log(event.target.src)
 }
 
 function drop (event) {
   event.preventDefault()
-  const src = event.dataTransfer.getData('src')
-  console.log(src)
+  const data = event.dataTransfer.getData('text')
+  console.log(data)
 }
 
 function dragOver (event) {
