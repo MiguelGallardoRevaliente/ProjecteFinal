7c0f4fd192a0ca3d154321329aac4ff9c659ef9d
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-05-15 13:10:04 +0200

    a

--- programacion/index.js
+++ programacion/index.js
@@ -490,18 +490,26 @@ app.get('/getCardsBattle', async (req, res) => {
     const [cartasCombatesUser] = await connection.execute('SELECT * FROM cartas_combates WHERE BIN_TO_UUID(id_user) = ? AND BIN_TO_UUID(id_combate) = ?;', [id, combate[0].id_combate_uuid])
     const cartasEnCombateUser = []
     for (const carta of cartasCombatesUser) {
-      const [cartaEnCombateUser] = await connection.execute('SELECT * FROM cartas WHERE id IN (?)', [carta.id_carta])
+      const [cartaEnCombateUser] = await connection.execute('SELECT * FROM cartas WHERE id = ?', [carta.id_carta])
       console.log(cartaEnCombateUser)
-      cartasEnCombateUser.push(cartaEnCombateUser[0])
+      const [ataque] = await connection.execute('SELECT * FROM ataques WHERE id = ?', [cartaEnCombateUser[0].id_ataque])
+      cartasEnCombateUser.push({
+        cartaEnCombateUser: cartaEnCombateUser[0],
+        ataque: ataque[0]
+      })
     }
     console.log(cartasEnCombateUser)
 
     const [cartasCombatesOpponent] = await connection.execute('SELECT * FROM cartas_combates WHERE BIN_TO_UUID(id_user) = ? AND BIN_TO_UUID(id_combate) = ?;', [opponent, combate[0].id_combate_uuid])
     const cartasEnCombateOpponent = []
     for (const carta of cartasCombatesOpponent) {
-      const [cartaEnCombateOpponent] = await connection.execute('SELECT * FROM cartas WHERE id IN (?)', [carta.id_carta])
+      const [cartaEnCombateOpponent] = await connection.execute('SELECT * FROM cartas WHERE id = ?', [carta.id_carta])
       console.log(cartaEnCombateOpponent)
-      cartasEnCombateOpponent.push(cartaEnCombateOpponent[0])
+      const [ataque] = await connection.execute('SELECT * FROM ataques WHERE id = ?', [cartaEnCombateOpponent[0].id_ataque])
+      cartasEnCombateOpponent.push({
+        cartaEnCombateOpponent: cartaEnCombateOpponent[0],
+        ataque: ataque[0]
+      })
     }
     console.log(cartasEnCombateOpponent)
 
