6d1c163a198df3490a706591b98b9b2c0c1f7b1f
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-05-16 09:49:07 +0200

    a

--- programacion/web/js/combat.js
+++ programacion/web/js/combat.js
@@ -1,40 +1,40 @@
-/* eslint-disable no-unused-vars */
-import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js'
+// /* eslint-disable no-unused-vars */
+// import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js'
 
-const socket = io()
+// const socket = io()
 
-function dragStart (event) {
-  event.dataTransfer.setData('text', event.target.id)
-  console.log(event.target.id)
-}
+// function dragStart (event) {
+//   event.dataTransfer.setData('text', event.target.id)
+//   console.log(event.target.id)
+// }
 
-function drop (event) {
-  event.preventDefault()
-  const data = event.dataTransfer.getData('text')
-  console.log(data)
-  const user = localStorage.getItem('username')
+// function drop (event) {
+//   event.preventDefault()
+//   const data = event.dataTransfer.getData('text')
+//   console.log(data)
+//   const user = localStorage.getItem('username')
 
-  socket.emit('playCard', { id: data, user })
+//   socket.emit('playCard', { id: data, user })
 
-  socket.on('played-card', (data) => {
-    console.log(data)
-    // Hacer que se muestre la carta en el board del user
-  })
+//   socket.on('played-card', (data) => {
+//     console.log(data)
+//     // Hacer que se muestre la carta en el board del user
+//   })
 
-  socket.on('not-in-match', () => {
-    console.log('You are not in a match')
-  })
+//   socket.on('not-in-match', () => {
+//     console.log('You are not in a match')
+//   })
 
-  socket.on('card-not-deck', () => {
-    console.log('You do not have that card in your deck')
-  })
+//   socket.on('card-not-deck', () => {
+//     console.log('You do not have that card in your deck')
+//   })
 
-  socket.on('not-your-turn', () => {
-    console.log('It is not your turn')
-    // Hay que hacer la comprovacion de los turnos y el cambio entre estos
-  })
-}
+//   socket.on('not-your-turn', () => {
+//     console.log('It is not your turn')
+//     // Hay que hacer la comprovacion de los turnos y el cambio entre estos
+//   })
+// }
 
-function dragOver (event) {
-  event.preventDefault()
-}
+// function dragOver (event) {
+//   event.preventDefault()
+// }
