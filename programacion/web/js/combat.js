863e7e087a0db012d891d84979a80de0cb84ff5a
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-05-16 11:34:05 +0200

    Update combat.js

--- programacion/web/js/combat.js
+++ programacion/web/js/combat.js
@@ -1,37 +1,14 @@
 /* eslint-disable no-unused-vars */
 
-function dragStart (event) {
-  event.dataTransfer.setData('text', event.target.id)
-  console.log(event.target.id)
-}
+// socket.on('not-in-match', () => {
+//   console.log('You are not in a match')
+// })
 
-function drop (event) {
-  event.preventDefault()
-  const data = event.dataTransfer.getData('text')
-  console.log(data)
-  const user = localStorage.getItem('username')
+// socket.on('card-not-deck', () => {
+//   console.log('You do not have that card in your deck')
+// })
 
-  socket.emit('playCard', { id: data, user })
-
-  socket.on('played-card', (data) => {
-    console.log(data)
-    // Hacer que se muestre la carta en el board del user
-  })
-
-  socket.on('not-in-match', () => {
-    console.log('You are not in a match')
-  })
-
-  socket.on('card-not-deck', () => {
-    console.log('You do not have that card in your deck')
-  })
-
-  socket.on('not-your-turn', () => {
-    console.log('It is not your turn')
-    // Hay que hacer la comprovacion de los turnos y el cambio entre estos
-  })
-}
-
-function dragOver (event) {
-  event.preventDefault()
-}
+// socket.on('not-your-turn', () => {
+//   console.log('It is not your turn')
+//   // Hay que hacer la comprovacion de los turnos y el cambio entre estos
+// })
