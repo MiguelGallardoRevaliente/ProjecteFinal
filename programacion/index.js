82c3e087057a5c46800eb57a5b9f98920d5e9931
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-05-20 18:46:02 +0200

    Update index.js

--- programacion/index.js
+++ programacion/index.js
@@ -218,6 +218,38 @@ io.on('connection', async (socket) => {
     }
 
     if (user[0].id_uuid === combate[0].id_user_1_uuid) {
+      const [cartasUser1] = await connection.execute('SELECT * FROM cartas_combates WHERE BIN_TO_UUID(id_user) = ? AND BIN_TO_UUID(id_combate) = ?;', [combate[0].id_user_1_uuid, combate[0].id_combate_uuid])
+      for (const carta of cartasUser1) {
+        const [cartaInfo] = await connection.execute('SELECT * FROM cartas WHERE id = ?;', [carta.id_carta])
+        if (carta.duracion_efecto <= 1 && carta.efecto_secundario) {
+          if (carta.efecto_secundario) {
+            if (carta.estadistica_efecto === 'ataque') {
+              await connection.execute('UPDATE cartas_combates SET ataque = ?, efecto_secundario = NULL, duracion_efecto = NULL, estadistica_efecto = NULL, cambio_estadistica = NULL WHERE id_carta = ? AND BIN_TO_UUID(id_combate) = ? AND BIN_TO_UUID(id_user) = ?;', [cartaInfo[0].ataque, carta.id_carta, combate[0].id_combate_uuid, combate[0].id_user_1_uuid])
+            }
+          }
+        } else if (carta.duracion_efecto && carta.duracion_efecto > 1) {
+          await connection.execute('UPDATE cartas_combates SET duracion_efecto = duracion_efecto - 1 WHERE id_carta = ? AND BIN_TO_UUID(id_combate) = ? AND BIN_TO_UUID(id_user) = ?;', [carta.id_carta, combate[0].id_combate_uuid, combate[0].id_user_1_uuid])
+        }
+      }
+    }
+
+    if (user[0].id_uuid === combate[0].id_user_2_uuid) {
+      const [cartasUser2] = await connection.execute('SELECT * FROM cartas_combates WHERE BIN_TO_UUID(id_user) = ? AND BIN_TO_UUID(id_combate) = ?;', [combate[0].id_user_2_uuid, combate[0].id_combate_uuid])
+      for (const carta of cartasUser2) {
+        const [cartaInfo] = await connection.execute('SELECT * FROM cartas WHERE id = ?;', [carta.id_carta])
+        if (carta.duracion_efecto && carta.duracion_efecto <= 1) {
+          if (carta.efecto_secundario) {
+            if (carta.estadistica_efecto === 'ataque') {
+              await connection.execute('UPDATE cartas_combates SET ataque = ?, efecto_secundario = NULL, duracion_efecto = NULL, estadistica_efecto = NULL, cambio_estadistica = NULL WHERE id_carta = ? AND BIN_TO_UUID(id_combate) = ? AND BIN_TO_UUID(id_user) = ?;', [cartaInfo[0].ataque, carta.id_carta, combate[0].id_combate_uuid, combate[0].id_user_2_uuid])
+            }
+          }
+        } else if (carta.duracion_efecto && carta.duracion_efecto > 1) {
+          await connection.execute('UPDATE cartas_combates SET duracion_efecto = duracion_efecto - 1 WHERE id_carta = ? AND BIN_TO_UUID(id_combate) = ? AND BIN_TO_UUID(id_user) = ?;', [carta.id_carta, combate[0].id_combate_uuid, combate[0].id_user_2_uuid])
+        }
+      }
+    }
+
+    if (user[0].id_uuid === combate[0].id_user_1_uuid) {
       await connection.execute('UPDATE combates SET turno = UUID_TO_BIN(?) WHERE BIN_TO_UUID(id_combate) = ?', [combate[0].id_user_2_uuid, combate[0].id_combate_uuid])
     } else if (user[0].id_uuid === combate[0].id_user_2_uuid) {
       await connection.execute('UPDATE combates SET turno = UUID_TO_BIN(?) WHERE BIN_TO_UUID(id_combate) = ?', [combate[0].id_user_1_uuid, combate[0].id_combate_uuid])
@@ -322,6 +354,9 @@ io.on('connection', async (socket) => {
                 'UPDATE cartas_combates SET vida = ?, efecto_secundario = ?, duracion_efecto = ?, estadistica_efecto = ?, cambio_estadistica = ? WHERE id_carta = ? AND BIN_TO_UUID(id_combate) = ? AND BIN_TO_UUID(id_user) = ?;',
                 [vida, tipoSplited[0], ataque[0].duracion, ataque[0].estadistica, ataque[0].cambio, carta.id_carta, combate[0].id_combate_uuid, user[0].id_uuid]
               )
+            } else {
+              io.emit('already-has-effect', { message: 'Already has effect', username })
+              return
             }
           } else if (ataque[0].estadistica === 'ataque') {
             if (!carta.efecto_secundario) {
@@ -330,6 +365,9 @@ io.on('connection', async (socket) => {
                 'UPDATE cartas_combates SET ataque = ?, efecto_secundario = ?, duracion_efecto = ?, estadistica_efecto = ?, cambio_estadistica = ? WHERE id_carta = ? AND BIN_TO_UUID(id_combate) = ? AND BIN_TO_UUID(id_user) = ?;',
                 [ataqueNumber, tipoSplited[0], ataque[0].duracion, ataque[0].estadistica, ataque[0].cambio, carta.id_carta, combate[0].id_combate_uuid, user[0].id_uuid]
               )
+            } else {
+              io.emit('already-has-effect', { message: 'Already has effect', username })
+              return
             }
           }
         }
@@ -346,6 +384,7 @@ io.on('connection', async (socket) => {
     }
   })
 
+  /* Aqui es cada vez que pulsan en pasar turno */
   socket.on('end-turn', async (data) => {
     const username = data.username
     const ataques = []
