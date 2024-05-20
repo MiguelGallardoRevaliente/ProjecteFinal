46d18a93a76c413d0078573947a05f37d4ee3a00
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-05-20 21:43:27 +0200

    a ver si funciona me cago en la puta

--- programacion/index.js
+++ programacion/index.js
@@ -383,7 +383,7 @@ io.on('connection', async (socket) => {
 
         await connection.execute('UPDATE combates SET turno = UUID_TO_BIN(?) WHERE BIN_TO_UUID(id_combate) = ?', [opponentId, combate[0].id_combate_uuid])
 
-        io.emit('ended-turn', { username, cartas })
+        io.emit('ended-turn', { username, cartas, tipo: tipoSplited[0] })
         io.emit('special-attacked-area', { opponent: username, username: opponent[0].user, opponentCards, mana })
       }
 
