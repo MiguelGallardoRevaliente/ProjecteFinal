9ca050cb0c44d114b0acc3fdb6b6490cae39907a
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-05-20 17:58:31 +0200

    Update index.js

--- programacion/index.js
+++ programacion/index.js
@@ -349,6 +349,7 @@ io.on('connection', async (socket) => {
   socket.on('end-turn', async (data) => {
     const username = data.username
     const ataques = []
+    console.log(username)
 
     const [user] = await connection.execute('SELECT *, BIN_TO_UUID(id) AS id_uuid FROM users WHERE user = ?', [username])
     const [combate] = await connection.execute('SELECT *, BIN_TO_UUID(id_combate) AS id_combate_uuid, BIN_TO_UUID(id_user_1) AS id_user_1_uuid, BIN_TO_UUID(id_user_2) AS id_user_2_uuid, BIN_TO_UUID(turno) as turno_uuid FROM combates WHERE BIN_TO_UUID(id_user_1) = ? OR BIN_TO_UUID(id_user_2) = ?;', [user[0].id_uuid, user[0].id_uuid])
