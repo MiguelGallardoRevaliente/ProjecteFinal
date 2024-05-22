4129bd07f244e408b18f3f40a4e766aa8fe48a70
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-05-22 09:59:15 +0200

    a

--- programacion/index.js
+++ programacion/index.js
@@ -676,6 +676,79 @@ io.on('connection', async (socket) => {
     }
   })
 
+  socket.on('special-attack-revive', async (data) => {
+    const idCartaAttacked = data.idCartaAttacked
+    const idCartaAttacking = data.idCartaAttacking
+    const username = data.username
+    const idAtaque = data.idAtaque
+
+    let opponentId
+    const ataques = []
+    const cartasInfo = []
+
+    const [ataque] = await connection.execute('SELECT * FROM ataques WHERE id = ?;', [idAtaque])
+    const [user] = await connection.execute('SELECT *, BIN_TO_UUID(id) AS id_uuid FROM users WHERE user = ?', [username])
+    const [combate] = await connection.execute('SELECT *, BIN_TO_UUID(id_combate) AS id_combate_uuid, BIN_TO_UUID(id_user_1) AS id_user_1_uuid, BIN_TO_UUID(id_user_2) AS id_user_2_uuid, BIN_TO_UUID(turno) as turno_uuid FROM combates WHERE BIN_TO_UUID(id_user_1) = ? OR BIN_TO_UUID(id_user_2) = ?;', [user[0].id_uuid, user[0].id_uuid])
+
+    if (user[0].id_uuid === combate[0].id_user_1_uuid) {
+      opponentId = combate[0].id_user_2_uuid
+    } else if (user[0].id_uuid === combate[0].id_user_2_uuid) {
+      opponentId = combate[0].id_user_1_uuid
+    }
+
+    const [cartasCombate] = await connection.execute('SELECT * FROM cartas_combates WHERE BIN_TO_UUID(id_user) = ? AND BIN_TO_UUID(id_combate) = ?;', [opponentId, combate[0].id_combate_uuid])
+    for (const cartaCombate of cartasCombate) {
+      const [carta] = await connection.execute('SELECT * FROM cartas WHERE id = ?;', [cartaCombate.id_carta])
+      const [ataque] = await connection.execute('SELECT * FROM ataques WHERE id = ?', [carta[0].id_ataque])
+      ataques.push(ataque[0])
+      cartasInfo.push(carta[0])
+    }
+
+    const cartas = {
+      cartasInfo,
+      cartasCombate,
+      ataques
+    }
+
+    if (ataque[0].nombre === 'Spectral Revive') {
+      const [cartaInfo] = await connection.execute(
+        'SELECT * FROM cartas WHERE id = ?;', [idCartaAttacked]
+      )
+
+      const [cartaCombate] = await connection.execute(
+        'SELECT * FROM cartas_combates WHERE id_carta = ? AND BIN_TO_UUID(id_user) = ? AND BIN_TO_UUID(id_combate) = ?;',
+        [idCartaAttacked, user[0].id_uuid, combate[0].id_combate_uuid]
+      )
+
+      if (cartaCombate[0].vida === 0) {
+        const vida = Math.round(cartaInfo[0].vida / 2)
+        console.log('Vida', vida)
+        await connection.execute(
+          'UPDATE cartas_combates SET vida = ? WHERE id_carta = ? AND BIN_TO_UUID(id_combate) = ? AND BIN_TO_UUID(id_user) = ?;',
+          [vida, idCartaAttacked, combate[0].id_combate_uuid, user[0].id_uuid]
+        )
+
+        await connection.execute(
+          'UPDATE cartas_combates SET ataque_especial = 1 WHERE id_carta = ? AND BIN_TO_UUID(id_combate) = ? AND BIN_TO_UUID(id_user) = ?;',
+          [idCartaAttacking, combate[0].id_combate_uuid, user[0].id_uuid]
+        )
+
+        const [cartasUser] = await connection.execute(
+          'SELECT * FROM cartas_combates WHERE BIN_TO_UUID(id_user) = ? AND BIN_TO_UUID(id_combate) = ?;',
+          [user[0].id_uuid, combate[0].id_combate_uuid]
+        )
+
+        await connection.execute(
+          'UPDATE combates SET turno = UUID_TO_BIN(?) WHERE BIN_TO_UUID(id_combate) = ?;',
+          [opponentId, combate[0].id_combate_uuid]
+        )
+
+        io.emit('ended-turn', { username, cartas })
+        io.emit('special-attacked-revive', { username, cartasUser })
+      }
+    }
+  })
+
   socket.on('special-attack-opponent', async (data) => {
     const idCartaAttacked = data.idCartaAttacked
     const idCartaAttacking = data.idCartaAttacking
