1589eb10c6e1d73f9b1b2fe0846da773493b0150
MiguelGallardoRevaliente <migare@jviladoms.cat>
2024-05-23 10:31:20 +0200

    Update index.js

--- programacion/index.js
+++ programacion/index.js
@@ -1651,27 +1651,42 @@ io.on('connection', async (socket) => {
   socket.on('leave-battle', async (data) => {
     const username = data.username
     const [user] = await connection.execute('SELECT *, BIN_TO_UUID(id) AS id_uuid FROM users WHERE user = ?', [username])
+    console.log('1')
     const [combate] = await connection.execute(
       'SELECT *, BIN_TO_UUID(id_combate) AS id_combate_uuid, BIN_TO_UUID(id_user_1) AS id_user_1_uuid, BIN_TO_UUID(id_user_2) AS id_user_2_uuid FROM combates WHERE BIN_TO_UUID(id_user_1) = ? OR BIN_TO_UUID(id_user_2) = ?;',
       [user[0].id_uuid, user[0].id_uuid]
     )
+    console.log('2')
 
     if (user[0].id_uuid === combate[0].id_user_1_uuid) {
+      console.log('3')
       await connection.execute('UPDATE users SET fighting = 0 WHERE BIN_TO_UUID(id) = ?', [combate[0].id_user_2_uuid])
+      console.log('4')
       const [opponent] = await connection.execute('SELECT * FROM users WHERE BIN_TO_UUID(id) = ?', [combate[0].id_user_2_uuid])
+      console.log('5')
       if (opponent[0].fighting === 0) {
+        console.log('6')
         await connection.execute('DELETE FROM cartas_combates WHERE BIN_TO_UUID(id_combate) = ?', [combate[0].id_combate_uuid])
+        console.log('7')
         await connection.execute('DELETE FROM combates WHERE BIN_TO_UUID(id_combate) = ?', [combate[0].id_combate_uuid])
+        console.log('8')
       }
     } else {
+      console.log('9')
       await connection.execute('UPDATE users SET fighting = 0 WHERE BIN_TO_UUID(id) = ?', [combate[0].id_user_1_uuid])
+      console.log('10')
       const [opponent] = await connection.execute('SELECT * FROM users WHERE BIN_TO_UUID(id) = ?', [combate[0].id_user_1_uuid])
+      console.log('11')
       if (opponent[0].fighting === 0) {
+        console.log('12')
         await connection.execute('DELETE FROM cartas_combates WHERE BIN_TO_UUID(id_combate) = ?', [combate[0].id_combate_uuid])
+        console.log('13')
         await connection.execute('DELETE FROM combates WHERE BIN_TO_UUID(id_combate) = ?', [combate[0].id_combate_uuid])
+        console.log('14')
       }
     }
 
+    console.log('15')
     await connection.execute(
       'UPDATE users SET oro = oro + ?, fighting = 0 WHERE BIN_TO_UUID(id) = ?',
       [data.reward, user[0].id_uuid]
